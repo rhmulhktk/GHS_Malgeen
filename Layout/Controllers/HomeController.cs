369ac0d01a1b715993bb67aa67618f60adfcc628
Raheem Ullah <raheemullah010101@gmail.com>
2024-06-06 08:57:52 +0500

    first commit

---
+++ Layout/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Layout.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AboutUs
+
+        [HttpGet]
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        // GET: Home
+
+        [HttpGet]
+        public ActionResult Staff()
+        {
+            return View();
+        }
+
+        // GET: Sports
+
+        [HttpGet]
+        public ActionResult Sports()
+        {
+            return View();
+        }
+
+        // GET: Galery
+
+        [HttpGet]
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+
+        // GET: Download
+
+        [HttpGet]
+        public ActionResult Download()
+        {
+            return View();
+        }
+
+        // GET: ContactUs
+
+        [HttpGet]
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+
+        // SET: ContactUs
+
+        [HttpPost]
+        public ActionResult ContactUs(string name, string e_mail, string message)
+        {
+            return View();
+        }
+
+
+    }
+
+    
+}
